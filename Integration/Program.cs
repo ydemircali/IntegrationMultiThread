9960d54662cbc216382aa1c05b7f3eaebcdece4f
Yakup Demircali <Yakup.Demircali@logo.com.tr>
2023-03-11 18:37:50 +0300

    devan

--- Integration/Program.cs
+++ Integration/Program.cs
@@ -13,7 +13,7 @@ namespace Integration {
             var t3 = Task.Run(async () => { await service.SaveItem("a"); });
             var t4 = Task.Run(async () => { await service.SaveItem("b"); });
             var t5 = Task.Run(async () => { await service.SaveItem("c"); });
-            Task.WaitAll(t0, t1, t2, t4, t5);
+            Task.WaitAll(t0, t1, t2, t3, t4, t5);
 
             Console.WriteLine("Everything recorded:");
 
